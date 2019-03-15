d3b1c641780ccdca5ea23dbaff23714d82cb1210
chbridges <47895194+chbridges@users.noreply.github.com>
2019-03-15 14:35:02 +0100

    Delete get_actions_from_rewards.py

--- agent_code/coinllector/get_actions_from_rewards.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-rewards = np.load('rewards.npy')
-last_actions = np.zeros((0,1), dtype=np.int16)
-for i in range(len(rewards)):
-    action = np.nonzero(rewards[i])[0][0]
-    last_actions = np.vstack((last_actions, [action]))
-print(rewards.shape, last_actions.shape)
-np.save('last_actions.npy', last_actions)
